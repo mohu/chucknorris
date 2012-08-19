054eaec376c7054f35a6164a6922cbef4156cbab
Rikki Pitt <rikki@studiomohu.com>
2012-08-19 23:02:02 +0100

    Added latest backup model

--- admin/models/backup.php
+++ admin/models/backup.php
@@ -1,28 +1,66 @@
 <?php
-require_once 'models/backup.php';
-$model  = new Model_Backup();
 
-$id     = (isset($_GET['id'])) ? $_GET['id'] : null;
-$action = (isset($_GET['action'])) ? $_GET['action'] : null;
+class Model_Backup extends RedBean_SimpleModel {
 
-/**
-* Delete view
-*/
-if ($action == 'delete' && $id) {
+  function fields() {
+    // Add fields here
+    $fields['file']       = array('type'=>'text', 'label'=>'file', 'max_length'=>'255', 'help'=>'');
+    $fields['date']       = array('type'=>'text', 'label'=>'date', 'max_length'=>'255', 'help'=>'');
+    
+    // Settings
+    $fields['add']        = false;
+    $fields['edit']       = false;
+    $fields['delete']     = true;
 
-  $dict['result']   = $model->trash($id);
-  $dict['data']     = $model->backup();
-  $dict['settings'] = $model->settings();
-  echo $twig->render( 'backup.html', $dict);
+    // Cron
+    $fields['run']        = array('path'=>'/admin/core/backup.php', 'button'=>'Run backup', 'button_running'=>'Backing up...');
 
-/**
-* List view
-*/
-} else {
+    return $fields;
+  }
 
-  $dict['data']       = $model->backup();
-  $dict['pagination'] = $model->count();
-  $dict['settings']   = $model->settings();
-  echo $twig->render( 'backup.html', $dict);
+  function backup() {
+    $start = ($_GET['start']) ? (int)$_GET['start'] : 0;
+    $limit = R::getCell('SELECT pagination FROM settings LIMIT 1');
 
+    $limit = ($limit) ? (int)$limit : 999999;
+
+    $data = R::getAll( 'SELECT *
+                        FROM backup
+                        LIMIT ' . $start . ', ' . $limit );
+
+    return $data;
+  }
+
+  function settings() {
+    $dict = App::getSettings($this->fields());
+    return $dict;
+  }
+
+  function view() {
+    global $module;
+    $dict = App::view($module, __CLASS__); // Region optional
+    return $dict;
+  }
+
+  function count() {
+    global $module;
+    $dict = App::count($module); // Region optional
+    return $dict;
+  }
+
+  function add() {
+    return App::buildForm($this->fields());
+  }
+
+  function edit($id) {
+    global $module;
+    sanitize($id);
+    return App::buildEditform($this->fields(), $module, $id);
+  }
+
+  function trash($id) {
+    global $module;
+    sanitize($id);
+    return App::trash($id, $module);
+  }
 }
