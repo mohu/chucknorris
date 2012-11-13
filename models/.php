310152a32bbf30a0d321d8eddc187e428f7b94e7
Rikki Pitt <rikki@studiomohu.com>
2012-11-13 16:59:58 +0000

    Removed old common.php model - not DRY

--- models/.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Model_ extends RedBean_SimpleModel {
-
-	function () {
-		$dict = array();
-		// Add database calls here
-		return $dict;
-	}
-}
--- models/common.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Model_Common extends RedBean_SimpleModel {
-
-	function common() {
-    $common = array();
-    return $common;
-	}
-}
