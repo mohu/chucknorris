ae46cec7d210a171c97e56ec818f6c168b6774f7
Rikki Pitt <rikki@studiomohu.com>
2013-02-10 18:18:27 +0000

    Updated twitter retrieval script for admin area

--- cron/twitter-cron.php
+++ cron/twitter-cron.php
@@ -1,67 +1,75 @@
 <?php
-require_once realpath(dirname(__FILE__).'/..'). '/includes/redbean/rb.php';
-require_once realpath(dirname(__FILE__).'/..'). '/includes/common/dbconnector.php';
-require_once realpath(dirname(__FILE__).'/..'). '/includes/common/functions.php';
+## Initialise core app
+require_once realpath(dirname(__FILE__).'/..') . '/' . 'admin/core/app.php';
+$app = new App();
 
-$site  = R::getCell('SELECT twitter FROM settings');
+$site  = ltrim(R::getCell('SELECT twitter FROM settings'), '@');
 
 echo saveTweets($site, 'tweets');
 
+// Update the below query if you want to get tweets from other admin members of your site
+// and include them in the twitter database table if required
 $team = R::getAll('SELECT twitter FROM users WHERE twitter IS NOT NULL');
 
 foreach ($team AS $team) {
     if ($team['twitter']) {
-        echo saveTweets($team['twitter'], 'tweets');
+        echo saveTweets(ltrim($team['twitter'], '@'), 'tweets');
     }
 }
 
 function saveTweets($screenname, $table) {
-    global $link;
 
-    $screenname = sanitize(strtolower(trim($screenname)));
-    
-    if (!$screenname) {
-      echo '<p>Error: No screen name declared.</p>';
-      return false;
-    }
+  $screenname = sanitize(strtolower(trim($screenname)));
 
-    $last_id = R::getCell('SELECT tid FROM '.$table.' WHERE screenname = "'. $screenname .'" ORDER BY tid DESC LIMIT 1');
+  if (!$screenname) {
+    echo '<p class="alert alert-error"><strong>Error</strong>: No screen name declared.<br>Add your site\'s Twitter username to the <a href="/admin/settings/">settings page</a>.</p>';
+    return false;
+  }
 
-    $url = 'http://api.twitter.com/1/statuses/user_timeline.xml?screen_name=' . $screenname;
-    if ($last_id) { $url .= '&since_id=' . $last_id; }
-    $ch = curl_init($url);
-    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    $xml = curl_exec ($ch);
-    curl_close ($ch);
+  $last_id = R::getCell('SELECT tid FROM '.$table.' WHERE screenname = "'. $screenname .'" ORDER BY tid DESC LIMIT 1');
 
-    $affected = 0;
-    $twelement = new SimpleXMLElement($xml);
+  $url = 'http://api.twitter.com/1/statuses/user_timeline.xml?screen_name=' . $screenname;
+  if ($last_id) { $url .= '&since_id=' . $last_id; }
+  $ch = curl_init($url);
+  curl_setopt ($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  $xml = curl_exec ($ch);
+  curl_close ($ch);
 
-    ## Reverse XML to save oldest to newest...
-    $twelement = array_reverse($twelement->xpath('status'));
+  $affected = 0;
+  $twelement = new SimpleXMLElement($xml);
 
-    foreach ($twelement as $status) {
-        $text = trim($status->text);
-        $text = str_replace(array("\r\n", "\r", "\n\n", "\n", "\t"), ' ', $text);
-        $time = strtotime($status->created_at);
-        $tid =  strval($status->id);
+  ## Any errors?
+  $error = ($twelement->xpath('error')) ? (string) (reset($twelement->xpath('error'))) : null;
 
-        ## Create Tweet
-        $tweet            = R::dispense($table); // Tweets
+  ## Reverse XML to save oldest to newest...
+  $twelement = array_reverse($twelement->xpath('status'));
 
-            $tweet->tid           = $tid;
-            $tweet->setMeta("cast.tid", "string"); // Cast to BIGINT
+  foreach ($twelement as $status) {
+    // Parse twitter xml
+    $text = trim($status->text);
+    $text = str_replace(array("\r\n", "\r", "\n\n", "\n", "\t"), ' ', $text);
+    $time = strtotime($status->created_at);
+    $tid =  strval($status->id);
 
-            $tweet->screenname   = $screenname;
-            $tweet->time          = $time;
-            $tweet->text          = $text;
-            $tweet->published     = 1;
-            
-        R::store($tweet);
-        unset($tweet);
+    ## Create Tweet
+    $tweet            = R::dispense($table); // Tweets
 
-        $affected++;
-    }
+      $tweet->tid           = $tid;
+      $tweet->setMeta("cast.tid", "string"); // Cast to BIGINT
 
-    return '<p>' . number_format($affected) . ' new tweets from <strong>'. $screenname .'</strong> saved.</p>';
+      $tweet->screenname = $screenname;
+      $tweet->time = $time;
+      $tweet->text = $text;
+      $tweet->published = 1;
+
+      R::store($tweet);
+      unset($tweet);
+
+      $affected++;
+  }
+
+  $message = '<p>' . number_format($affected) . ' new tweets from <strong>'. $screenname .'</strong> saved.</p>';
+  if ($error) $message .= '<p class="alert alert-error"><small>' . $error . '</small></p>';
+
+  return $message;
 }
