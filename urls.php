f0626c1f2296319f2e75d0ff87aed09c6ea6d25f
Rikki Pitt <rikki@studiomohu.com>
2013-02-11 17:15:14 +0000

    Added trailing slashes by .htaccess for urls.php

--- urls.php
+++ urls.php
@@ -4,7 +4,8 @@ $valid_paths = array(
   // Home page
   '^/$' => array('home', 'home'),
   // Examples
-  '^/about$' => array('about', 'about'),
-  '^/blog/(?<page>\d+)/(?<slug>[\w-]+)$' => array('blog', 'blog'),
-  '^/blog/(?<slug>[\w-]+)$' => array('blog', 'post'),
+  '^/about/$' => array('about', 'about'),
+  '^/(?<year>\d{4})/(?<month>\w{1,2})/(?<slug>[0-9A-Za-z-]+)/$' => array('blog', 'post'),
+  '^/(?<year>\d{4})/(?<month>\w{1,2})/$' => array('blog', 'blog'),
+  '^/(?<year>\d{4})/$' => array('blog', 'blog'),
 );
